bcc1464fe4cbdbab8a4da894b79b3d38e122a22e
robiel <robiel.tesfazghi@cpnv.ch>
2023-02-23 22:32:08 +0100

    update DB

--- db/note.sql
+++ db/note.sql
@@ -25,9 +25,9 @@ CREATE TABLE IF NOT EXISTS `mydb`.`category` (
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`class`
+-- Table `mydb`.`lecture`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`class` (
+CREATE TABLE IF NOT EXISTS `mydb`.`lecture` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(60) NOT NULL,
   `description` TEXT NOT NULL,
@@ -46,32 +46,49 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`person`
+-- Table `mydb`.`type_person`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`person` (
+CREATE TABLE IF NOT EXISTS `mydb`.`type_person` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(50) NOT NULL,
-  `last_name` VARCHAR(50) NOT NULL,
-  `email` VARCHAR(60) NOT NULL,
-  `phone` VARCHAR(20) NOT NULL,
+  `name` VARCHAR(60) NOT NULL,
+  `slug` VARCHAR(5) NOT NULL,
+  `description` TEXT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`student`
+-- Table `mydb`.`person`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`student` (
+CREATE TABLE IF NOT EXISTS `mydb`.`person` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `person_id` INT NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `firstname` VARCHAR(50) NOT NULL,
+  `lastname` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(60) NOT NULL,
+  `phone` VARCHAR(20) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT 'CURRENT_TIMESTAMP',
+  `type_person_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_student_person1_idx` (`person_id` ASC) VISIBLE,
-  CONSTRAINT `fk_student_person`
-    FOREIGN KEY (`person_id`)
-    REFERENCES `mydb`.`person` (`id`)
+  INDEX `fk_person_type_person1_idx` (`type_person_id` ASC) VISIBLE,
+  CONSTRAINT `fk_person_type_person1`
+    FOREIGN KEY (`type_person_id`)
+    REFERENCES `mydb`.`type_person` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`promotion`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`promotion` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(60) NOT NULL,
+  `start_date` DATE NOT NULL,
+  `end_date` DATE NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -87,15 +104,63 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`teacher`
+-- Table `mydb`.`grade`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`teacher` (
+CREATE TABLE IF NOT EXISTS `mydb`.`grade` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `person_id` INT NOT NULL,
+  `name` VARCHAR(120) NULL,
+  `result` DECIMAL(1,1) NOT NULL,
+  `weight` DECIMAL(3,2) NOT NULL,
+  `date` DATE NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `semester_id` INT NOT NULL,
+  `promotion_id` INT NOT NULL,
+  `student_id` INT NOT NULL,
+  `teacher_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_student_person1_idx` (`person_id` ASC) VISIBLE,
-  CONSTRAINT `fk_teacher_person`
+  INDEX `fk_exam_semester1_idx` (`semester_id` ASC) VISIBLE,
+  INDEX `fk_exam_academic_year1_idx` (`promotion_id` ASC) VISIBLE,
+  INDEX `fk_grade_person1_idx` (`student_id` ASC) VISIBLE,
+  INDEX `fk_grade_person2_idx` (`teacher_id` ASC) VISIBLE,
+  CONSTRAINT `fk_exam_semester`
+    FOREIGN KEY (`semester_id`)
+    REFERENCES `mydb`.`semester` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_exam_promotion`
+    FOREIGN KEY (`promotion_id`)
+    REFERENCES `mydb`.`promotion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_grade_person1`
+    FOREIGN KEY (`student_id`)
+    REFERENCES `mydb`.`person` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_grade_person2`
+    FOREIGN KEY (`teacher_id`)
+    REFERENCES `mydb`.`person` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`person_promotion`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`person_promotion` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `promotion_id` INT NOT NULL,
+  `person_id` INT NOT NULL,
+  INDEX `fk_person_has_academic_year_academic_year1_idx` (`promotion_id` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
+  INDEX `fk_person_promotion_person1_idx` (`person_id` ASC) VISIBLE,
+  CONSTRAINT `fk_student_promotion_promotion`
+    FOREIGN KEY (`promotion_id`)
+    REFERENCES `mydb`.`promotion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_person_promotion_person1`
     FOREIGN KEY (`person_id`)
     REFERENCES `mydb`.`person` (`id`)
     ON DELETE NO ACTION
@@ -104,138 +169,60 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`exam`
+-- Table `mydb`.`lecture_grade`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`exam` (
+CREATE TABLE IF NOT EXISTS `mydb`.`lecture_grade` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(120) NULL,
-  `result` DECIMAL(1,1) NOT NULL,
-  `percentage` DECIMAL(2,2) NOT NULL,
-  `date` DATE NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `semester_id` INT NOT NULL,
-  `student_id` INT NOT NULL,
-  `teacher_id` INT NOT NULL,
+  `lecture_id` INT NOT NULL,
+  `grade_id` INT NOT NULL,
+  INDEX `fk_class_has_exam_exam1_idx` (`grade_id` ASC) VISIBLE,
+  INDEX `fk_class_has_exam_class1_idx` (`lecture_id` ASC) VISIBLE,
   PRIMARY KEY (`id`),
-  INDEX `fk_exam_semester1_idx` (`semester_id` ASC) VISIBLE,
-  INDEX `fk_exam_student1_idx` (`student_id` ASC) VISIBLE,
-  INDEX `fk_exam_teacher1_idx` (`teacher_id` ASC) VISIBLE,
-  CONSTRAINT `fk_exam_semester`
+  CONSTRAINT `fk_lecture_grade_lecture`
+    FOREIGN KEY (`lecture_id`)
+    REFERENCES `mydb`.`lecture` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lecture_grade_grade`
+    FOREIGN KEY (`grade_id`)
+    REFERENCES `mydb`.`grade` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`lecture_promotion`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`lecture_promotion` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `lecture_id` INT NOT NULL,
+  `promotion_id` INT NOT NULL,
+  `semester_id` INT NOT NULL,
+  `person_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_person_has_class_class1_idx` (`lecture_id` ASC) VISIBLE,
+  INDEX `fk_person_has_class_academic_year1_idx` (`promotion_id` ASC) VISIBLE,
+  INDEX `fk_lecture_promotion_semester1_idx` (`semester_id` ASC) VISIBLE,
+  INDEX `fk_lecture_promotion_person1_idx` (`person_id` ASC) VISIBLE,
+  CONSTRAINT `fk_lecture_promotion_class`
+    FOREIGN KEY (`lecture_id`)
+    REFERENCES `mydb`.`lecture` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lecture_promotion_promotion`
+    FOREIGN KEY (`promotion_id`)
+    REFERENCES `mydb`.`promotion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lecture_promotion_semester1`
     FOREIGN KEY (`semester_id`)
     REFERENCES `mydb`.`semester` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_exam_student`
-    FOREIGN KEY (`student_id`)
-    REFERENCES `mydb`.`student` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_exam_teacher`
-    FOREIGN KEY (`teacher_id`)
-    REFERENCES `mydb`.`teacher` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`academic_year`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`academic_year` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(60) NOT NULL,
-  `start_date` DATE NOT NULL,
-  `end_date` DATE NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `exam_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_academic_year_exam1_idx` (`exam_id` ASC) VISIBLE,
-  CONSTRAINT `fk_academic_year_exam`
-    FOREIGN KEY (`exam_id`)
-    REFERENCES `mydb`.`exam` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`person_academic_year`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`person_academic_year` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `academic_year_id` INT NOT NULL,
-  `student_id` INT NOT NULL,
-  INDEX `fk_person_has_academic_year_academic_year1_idx` (`academic_year_id` ASC) VISIBLE,
-  PRIMARY KEY (`id`),
-  INDEX `fk_person_academic_year_student1_idx` (`student_id` ASC) VISIBLE,
-  CONSTRAINT `fk_student_academic_year_academic_year`
-    FOREIGN KEY (`academic_year_id`)
-    REFERENCES `mydb`.`academic_year` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_student_academic_year_student`
-    FOREIGN KEY (`student_id`)
-    REFERENCES `mydb`.`student` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`class_exam`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`class_exam` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `class_id` INT NOT NULL,
-  `exam_id` INT NOT NULL,
-  INDEX `fk_class_has_exam_exam1_idx` (`exam_id` ASC) VISIBLE,
-  INDEX `fk_class_has_exam_class1_idx` (`class_id` ASC) VISIBLE,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_class_exam_class`
-    FOREIGN KEY (`class_id`)
-    REFERENCES `mydb`.`class` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_class_exam_exam`
-    FOREIGN KEY (`exam_id`)
-    REFERENCES `mydb`.`exam` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`class_academic_year`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`class_academic_year` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `class_id` INT NOT NULL,
-  `academic_year_id` INT NOT NULL,
-  `semester_id` INT NOT NULL,
-  `teacher_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_person_has_class_class1_idx` (`class_id` ASC) VISIBLE,
-  INDEX `fk_person_has_class_academic_year1_idx` (`academic_year_id` ASC) VISIBLE,
-  INDEX `fk_person_has_class_semester1_idx` (`semester_id` ASC) VISIBLE,
-  INDEX `fk_person_has_class_teacher1_idx` (`teacher_id` ASC) VISIBLE,
-  CONSTRAINT `fk_class_academic_year_class`
-    FOREIGN KEY (`class_id`)
-    REFERENCES `mydb`.`class` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_class_academic_year_academic_year`
-    FOREIGN KEY (`academic_year_id`)
-    REFERENCES `mydb`.`academic_year` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_class_academic_year_semester`
-    FOREIGN KEY (`semester_id`)
-    REFERENCES `mydb`.`semester` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_class_academic_year_teacher`
-    FOREIGN KEY (`teacher_id`)
-    REFERENCES `mydb`.`teacher` (`id`)
+  CONSTRAINT `fk_lecture_promotion_person1`
+    FOREIGN KEY (`person_id`)
+    REFERENCES `mydb`.`person` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
