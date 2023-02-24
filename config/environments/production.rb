545af8b2956965bfce9871e65f63e1170156592d
robiel <robiel.tesfazghi@cpnv.ch>
2023-02-24 14:08:23 +0100

    :building_construction: Rename the project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "sandbox_production"
+  # config.active_job.queue_name_prefix = "asp1_es_gifi_production"
 
   config.action_mailer.perform_caching = false
 
