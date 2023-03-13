6ac0f60429b7c8c5e3e2b072d22b5e9366ce80fe
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-13 15:46:15 +0100

    Add student, teacher, promotion and semester name to grades

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -10,4 +10,25 @@ class Grade < ApplicationRecord
   validates :result, presence: true, numericality: { greater_than_or_equal_to: 1.0, less_than_or_equal_to: 6.0 }
   validates :weight, presence: true
   validates :date, presence: true
+  validates :semester_id, presence: true
+  validates :promotion_id, presence: true
+  validates :student_id, presence: true
+  validates :teacher_id, presence: true
+
+  def student_name
+    Person.find(student_id).firstname + ' ' + Person.find(student_id).lastname
+  end
+
+  def teacher_name
+    Person.find(teacher_id).firstname + ' ' + Person.find(teacher_id).lastname
+  end
+
+  def semester_name
+    Semester.find(semester_id).name
+  end
+
+  def promotion_name
+    Promotion.find(promotion_id).name
+  end
+
 end
